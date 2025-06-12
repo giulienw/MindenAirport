de08f2bfc1e869a9492bf33c7b8431b0673fd64d
Giulien <giulienchow@gmail.com>
2025-06-12 09:28:10 +0200

    fixed CORS error

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,4 +7,13 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), tailwindcss()],
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      }
+    }
+  },
 });
